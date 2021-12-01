cb7e79a6e8634a36138b1a4853d8cac32d3ef9cd
pedro <phbs1235@gmail.com>
2021-12-01 16:13:01 -0300

    criação de pasta e arquivo

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const http = require('http')
+const fileSystem = require('fs')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
-    if(req.url === '/' && req.method === 'GET') {
-        console.log('Método get')
-        res.end(JSON.stringify({ message: 'Não deu' }))
+    if(req.url === '/' && req.method === 'POST') {
+        function createFolder(folder){
+            if(!fileSystem.existsSync(folder)){
+                fileSystem.mkdirSync(folder)
+                return 'Criou a pasta'
+            }
+            return 'Não criou a pasta'
+        }
+        const folder = 'repositorio'
+        const file = 'text.txt'
+        fileSystem.writeFileSync(folder+'/'+file, 'Deu certo')
+        try {
+            const newFolder = createFolder(folder)
+            res.end(JSON.stringify({ message: newFolder }))
+
+        } catch (error) {
+            res.end(JSON.stringify({ message: 'Deu algum erro', error }))
+        }
     } else {
         res.writeHead(404, { 'Content-Type': 'application/json' })
         res.end(JSON.stringify({ message: 'Rota não encontrada' }))
     }
 })
 
-const PORT =  process.env.PORT || 5000
+const PORT =  process.env.PORT || 3333
 
 server.listen(PORT, () => console.log(`Servidor rodando na porta: ${PORT}`))
 
