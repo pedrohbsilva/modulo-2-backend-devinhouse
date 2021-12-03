5ff5a1e36ae1a138a77a04647c80c56716786855
pedro <phbs1235@gmail.com>
2021-12-03 20:19:56 -0300

    example function for tests

--- src/server.js
+++ src/server.js
@@ -1,28 +1,31 @@
 const express = require('express');
 
 const app = express()
+const fileSystem = require('fs')
 const {createFolder} = require('./utils')
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.status(200).json({message: 'Hello world'})
+   return res.status(200).json({message: 'Hello world'})
 })
 
-app.post('/teste',  (req, res) => {
+app.post('/',  (req, res) => {
     const { folder, item } = req.body
+    const { job } = req.query
     const existFolder = createFolder(folder)
     delete req.body.folder
 
-    //Este if verifica se existe o arquivo input.json
-    if (fs.lstatSync(folder+'/'+'input.json').isFile()) {
-        //Neste result, retorna os dados do JSON de dentro do Input
-        const result = JSON.parse(fs.readFileSync(folder+'/'+'input.json', 'utf8'));
+    //Este if verifica se existe o arquivo user.json
+    if (fileSystem.lstatSync('src/'+'user.json').isFile()) {
+        //Neste result, retorna os dados do JSON de dentro do user
+        const result = JSON.parse(fileSystem.readFileSync('src/'+'user.json', 'utf8'));
         console.log(result)
+        return res.status(201).json({message: 'Caiu aqui'})
     }
-    //Nesta Linha de baixo, é criado o input.json e preenche o arquivo JSON de acordo com o que foi enviado no req.body
-    fs.writeFileSync(folder+'/'+'input.json', JSON.stringify(req.body));
+    //Nesta Linha de baixo, é criado o user.json e preenche o arquivo JSON de acordo com o que foi enviado no req.body
+    fileSystem.writeFileSync('src/'+'user.json', JSON.stringify(req.body));
 
-    res.status(201).json({message: 'Hello world'})
+    return res.status(201).json({message: 'Hello world'})
 })
 
 app.listen(3333, () => console.log('Executando'))
