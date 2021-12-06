fe1976493cbcf688152f71f851725774ff3d81e5
pedro <phbs1235@gmail.com>
2021-12-06 18:05:49 -0300

    filesystem

--- src/server.js
+++ src/server.js
@@ -2,29 +2,33 @@ const express = require('express');
 
 const app = express()
 const fileSystem = require('fs')
-const {createFolder} = require('./utils')
+const { createFolder } = require('./utils')
 app.use(express.json())
+const multer = require('multer')
 
 app.get('/', (req, res) => {
    return res.status(200).json({message: 'Hello world'})
 })
 
-app.post('/',  (req, res) => {
+// const destinyFolder = multer.diskStorage({
+//     destination: (req, file, callback) => {
+//         callback(null, 'src/folder')
+//     },
+//     filename: (req, file, callback) => {
+//         callback(null, Date.now()+file.originalname)
+//     }
+// })
+
+multer({dest: 'src/folder'})
+const upload = multer()
+
+
+app.post('/', upload.single('file'),  (req, res) => {
     const { folder, item } = req.body
-    const { job } = req.query
-    const existFolder = createFolder(folder)
-    delete req.body.folder
-
-    //Este if verifica se existe o arquivo user.json
-    if (fileSystem.lstatSync('src/'+'user.json').isFile()) {
-        //Neste result, retorna os dados do JSON de dentro do user
-        const result = JSON.parse(fileSystem.readFileSync('src/'+'user.json', 'utf8'));
-        console.log(result)
-        return res.status(201).json({message: 'Caiu aqui'})
-    }
-    //Nesta Linha de baixo, é criado o user.json e preenche o arquivo JSON de acordo com o que foi enviado no req.body
-    fileSystem.writeFileSync('src/'+'user.json', JSON.stringify(req.body));
-
+    
+    // const { job } = req.query
+    // const existFolder = createFolder(folder)
+    fileSystem.writeFileSync('src/folder/'+req.file.originalname, req.file.buffer)
     return res.status(201).json({message: 'Hello world'})
 })
 
